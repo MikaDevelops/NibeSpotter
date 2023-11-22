52b206d781fd43c792c45e50e5e11d85a8d8c3d4
mikavirtala <Mika.1.virtala@edu.karelia.fi>
2023-11-22 19:56:05 +0200

    front to check if token valid

--- front/src/App.js
+++ front/src/App.js
@@ -1,30 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import axios from 'axios';
+import Alert from './components/Alert';
 
 class App extends React.Component {
 
   constructor(props){
     super(props);
+    this.state = {
+      tokenValid: null
+    };
+    this.checkTokenValid = this.checkTokenValid.bind(this);
+  }
 
+  componentDidMount(){
+    this.checkTokenValid();
+  }
+
+  checkTokenValid(){
+    axios.get('http://localhost:3001/checktokenstatus').then((res)=>{
+      this.setState({tokenValid: res.data.tokenIsValid});
+    })
+    .catch(err=>{
+      console.log('Error message (checkTokenValid): ' + err.message);
+    });
   }
 
   render(){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+        <Alert tokenValid={this.state.tokenValid} />
+        
       </header>
     </div>
     );
