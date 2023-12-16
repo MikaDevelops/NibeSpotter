51066cde4466584afb11ff1ce85e0525a6539806
mikavirtala <Mika.1.virtala@edu.karelia.fi>
2023-12-16 21:06:28 +0200

    ventilation information to front

--- front/src/App.js
+++ front/src/App.js
@@ -2,19 +2,31 @@ import './App.css';
 import React from 'react';
 import axios from 'axios';
 import Alert from './components/Alert';
+import Air from './components/Air';
 
 class App extends React.Component {
 
   constructor(props){
     super(props);
+
     this.state = {
-      tokenValid: null
+      sysInfoUpdateInterval: 180,
+      tokenValid: null,
+      systemInfo: {
+        fanSpeed: "no info",
+        exhaustAir: "no info",
+        extractAir: "no info",
+        supplyAir: "no info"
+      }
     };
+
     this.checkTokenValid = this.checkTokenValid.bind(this);
+    this.updateSystemInfo = this.updateSystemInfo.bind(this);
   }
 
   componentDidMount(){
     this.checkTokenValid();
+    this.updateSystemInfo();
   }
 
   checkTokenValid(){
@@ -26,17 +38,42 @@ class App extends React.Component {
     });
   }
 
+  updateSystemInfo(){
+    axios.get('http://localhost:3001/getsystemstatus').then((res)=>{
+      let newdata = {
+        fanSpeed: res.data.systemStatus[1].parameters[0].displayValue,
+        exhaustAir: res.data.systemStatus[1].parameters[1].displayValue,
+        extractAir: res.data.systemStatus[1].parameters[2].displayValue,
+        supplyAir: res.data.systemStatus[1].parameters[3].displayValue,
+      };
+      this.setState({systemInfo: newdata});
+      if (this.state.sysInfoUpdateInterval > 0) setTimeout(()=>{this.updateSystemInfo()}, this.state.sysInfoUpdateInterval*1000);
+    });
+  }
+
   render(){
+
   return (
     <div className="App">
       <header className="App-header">
-       
         <Alert tokenValid={this.state.tokenValid} />
-        
       </header>
+
+      <Air 
+        airSupplyTemp={this.state.systemInfo.supplyAir}
+        airExtractTemp={this.state.systemInfo.extractAir}
+        airExhaustTemp={this.state.systemInfo.exhaustAir}
+        fanSpeed={this.state.systemInfo.fanSpeed}
+          />
     </div>
     );
   }
 }
 
 export default App;
+/*
+           airSupplyTemp={this.state.systemInfo.systemStatus[1].parameters[3].displayValue}
+          airExtractTemp={}
+          airExhaustTemp={}
+          fanSpeed={}
+*/
