a8cc3bb8ece3e15b41238d532aabdb2276867078
mikavirtala <Mika.1.virtala@edu.karelia.fi>
2024-01-19 23:00:31 +0200

    Database functions to Db class. Testing db-functions

---
+++ dataBase/Db.js
@@ -0,0 +1,175 @@
+class Db{
+ 
+    #typeOfDb;
+    #dataBaseAddress;
+    #spotDataModel;
+    
+    /**
+     * Creates Db instance. Default database 'sqlite'.
+     * @param {string} typeOfDb 'sqlite' or 'mariadb' 
+     * @param {string} dataBaseAddress default is for sqlite (db/nibespotter.db)
+     */
+    constructor(typeOfDb='sqlite', dataBaseAddress = '.db/nibespotter.db'){
+        if (typeOfDb !== 'sqlite' && typeOfDb !== 'mariadb'){
+            throw new Error('Db constructor: Not supported db-solution.');
+        }
+        this.#typeOfDb = typeOfDb;
+        this.#dataBaseAddress = dataBaseAddress;
+        const {spotDataModel} = require('./dataModel');
+        this.#spotDataModel = spotDataModel;
+    }
+
+    start(){
+
+        switch(this.#typeOfDb) {
+
+            case 'sqlite':
+                const db = this.#openDatabase();
+                this.#createTables(db);
+                this.#closeDataBase(db);
+            break;
+
+            case 'mariadb':
+                // TODO
+                console.log("Mariadb not suported yet.")
+            break;
+            
+            default:
+                throw new Error ('Database.start() type of database not supported.')
+
+        }
+    }
+
+    /**
+     * Checks exactly if table is in the database.
+     * @param {string} tableName
+     * @returns {Promise<boolean>} Promise that will resolve to true if table is found.
+     */
+    checkIfTableExists(tableName){
+
+        if(this.#typeOfDb === 'sqlite'){
+
+            return new Promise((resolve,reject)=>{
+                const db = this.#openDatabase();
+                let sqlString = `SELECT name FROM sqlite_master WHERE name = '${tableName}';`;
+                
+                db.get(sqlString,(err,row) => {
+                    
+                    if(err){
+                        console.log(err);
+                        reject(err);
+                    }
+                    
+                    if (row) resolve(true);
+                    if (!row) resolve(false);
+                });
+                this.#closeDataBase(db);
+            })
+        }
+    }
+
+    saveSpotData(data){
+        if (data.length > 100) {
+            throw new Error ('spot data too long');
+
+        }else{
+            const sqlString = this.#makeSqlInsertString(data);
+        }
+    }
+
+    #openDatabase(){
+        const sqlite = require('sqlite3').verbose();
+        const dataBase = new sqlite.Database(this.#dataBaseAddress, (error) => {
+            if(error) console.log(error.message);
+        });
+        return dataBase;
+    }
+
+    #closeDataBase(db){
+        db.close();
+    }
+
+    #createTables(newDB){
+   
+        newDB.exec(`
+        CREATE TABLE IF NOT EXISTS spotPrice
+        (
+            startTime DATETIME NOT NULL,
+            endTime DATETIME NOT NULL,
+            price FLOAT NOT NULL,
+            priceArea VARCHAR(50) NOT NULL,
+            PRIMARY KEY (startTime)
+        );
+       
+        CREATE TABLE IF NOT EXISTS location
+        (
+            longtitude FLOAT NOT NULL,
+            latitude FLOAT NOT NULL,
+            name VARCHAR(80) NOT NULL,
+            id INTEGER PRIMARY KEY
+        );
+    
+        CREATE TABLE IF NOT EXISTS nibeSystem
+        (
+            id INTEGER PRIMARY KEY NOT NULL,
+            productName VARCHAR(80) NOT NULL,
+            serialNumber VARCHAR(80) NOT NULL,
+            nibeStatusID INT NOT NULL,
+            locationID INT NOT NULL,
+            FOREIGN KEY (locationID) REFERENCES location(id)
+        );
+    
+        CREATE TABLE IF NOT EXISTS nibeStatus
+        (
+            time DATETIME NOT NULL,
+            roomTemperatureC FLOAT NOT NULL,
+            outdoorTemperatureC FLOAT NOT NULL,
+            id INTEGER PRIMARY KEY NOT NULL,
+            hotWaterTemperatureC FLOAT NOT NULL,
+            airFanSpeed INT NOT NULL,
+            nibeSystemID INT NOT NULL,
+            FOREIGN KEY (nibeSystemID) REFERENCES nibeSystem(id)
+        );
+    
+        CREATE TABLE IF NOT EXISTS settingHistory
+        (
+            time DATETIME NOT NULL,
+            id INTEGER PRIMARY KEY NOT NULL,
+            roomTargetTempC FLOAT NOT NULL,
+            ventilationBoost BOOLEAN NOT NULL,
+            nibeSystemID INT NOT NULL,
+            FOREIGN KEY (nibeSystemID) REFERENCES nibeSystem(id)
+        );
+    
+        CREATE TABLE IF NOT EXISTS weatherInfo
+        (
+            time DATETIME NOT NULL,
+            temperatureC FLOAT NOT NULL,
+            id INTEGER PRIMARY KEY NOT NULL,
+            serviceName VARCHAR(80) NOT NULL,
+            locationID INT NOT NULL,
+            FOREIGN KEY (locationID) REFERENCES location(id)
+        );
+        `);
+    }
+
+    #makeSqlInsertString(data){
+
+        let valuesString = '';
+
+        for(let i=0; i < data.length; i+=3){
+            valuesString += `('${data[i]}','${data[i+1]}','${data[i+2]}')`;
+            if(i < data.length-3) valuesString += ',';
+        }
+
+        let insertString = `INSERT INTO ${this.#spotDataModel.tableName}(
+            ${this.#spotDataModel.idField},${this.#spotDataModel.dataFields[0]},
+            ${this.#spotDataModel.dataFields[1]}, ${this.#spotDataModel.dataFields[2]})
+            VALUES `+valuesString +';';
+        
+        console.log(insertString);
+        return insertString;
+    }
+}
+
+module.exports={Db};
--- dataBase/dbTesting.js
+++ dataBase/dbTesting.js
@@ -1,5 +1,8 @@
 require('dotenv').config({path: '../.env'});
-const {saveSpotData} = require('./databaseOperations.js');
+const { setTimeout } = require('timers/promises');
+const database = require('./Db.js');
+const path = require('path');
+
 
 let dataset = ['2000-01-02T16:00:00','2000-01-02T17:00:00','6.66',
 '2000-01-02T17:00:00','2000-01-02T17:00:00','6.67',
@@ -8,12 +11,28 @@ let dataset = ['2000-01-02T16:00:00','2000-01-02T17:00:00','6.66',
 
 
 // test('database test', ()=> {
+//saveSpotData(dataset);
 
-saveSpotData(dataset);
+let pathToDb = path.resolve('..', 'db', 'nibespotter.db');
+const db = new database.Db('sqlite',pathToDb);
 
+// start procedure
+db.start();
 
+db.checkIfTableExists('location').then(
+    (value)=>{
+        console.log(value);
+    },
+    (error)=>{console.log(error);}
+).catch((error)=>{console.log(error)});
 
+db.checkIfTableExists('locaftion').then(
+    (value)=>{
+        console.log(value);
+    },
+    (error)=>{console.log(error);}
+).catch((error)=>{console.log(error)});
 
-// });
-
-
+// save data to the database
+db.saveSpotData(dataset);
+// check that saved data is get from database
