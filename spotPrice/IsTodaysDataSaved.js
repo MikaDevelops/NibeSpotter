bad56216b53d1c320578d6d88bafa446b2492a69
mikavirtala <Mika.1.virtala@edu.karelia.fi>
2024-01-25 09:31:25 +0200

    isTodaysDataAlreadySaved function refactor

--- spotPrice/IsTodaysDataSaved.js
+++ spotPrice/IsTodaysDataSaved.js
@@ -1,23 +1,24 @@
 const { Db } = require("../dataBase/Db");
-const { getTomorrow } = require("./DateTimeFunctions");
+const { getTomorrow,getMonthWithLeadZero } = require("./DateTimeFunctions");
 
 async function isTodaysDataSaved() {
 
     const today = new Date();
     const tomorrow = getTomorrow(today);
     tomorrow.setHours(12,0,0,0);
+    const dateString = 
+        tomorrow.getFullYear() + '-'
+        + getMonthWithLeadZero(tomorrow) + '-'
+        + tomorrow.getDate() + 'T12:00:00';
 
     const db = new Db();
     db.start();
 
-    const previousData = await db.getLatest24h();
+    const previousData = await db.getSpotData([dateString]);
 
-    if (previousData.length < 1) return false;
     if (previousData.length > 0) {
-        for (let i = 0; i < previousData.length; i++){
-            let date = new Date(previousData[i].startTime)
+            let date = new Date(previousData[0].startTime)
             if(date.valueOf() == tomorrow.valueOf()) return true;
-        }
     }
     return false;
 }
